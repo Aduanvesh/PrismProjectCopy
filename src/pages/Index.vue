5fbdb04186f5d0bf7f835dbd3d6a21304bb566ed
polarbeared <edwardw1997@gmail.com>
2020-08-11 14:11:04 +1000

    Added an insecure database connection sample.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,34 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
+    <q-item
+      v-for="item in name"
+      :key="item.id"
     >
+      {{'Hi, ' + item.first_name
+      }}
+      <br>
+      {{'Your password is: ' + item.pass
+      }}
+      <br>
+      {{'Your account was created: ' + item.date_created
+      }}
+      <br>
+      {{'You live at: ' + item.street_address + ' ' + item.suburb
+      }}
+    </q-item>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      name: []
+    }
+  },
+  created () {
+    this.$bind('name', this.$firestore.collection('users'))
+  }
 }
 </script>
