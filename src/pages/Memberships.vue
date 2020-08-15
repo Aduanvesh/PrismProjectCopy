eef5979449a64ccd5e6d201069322d4ec7d94ab9
polarbeared <edwardw1997@gmail.com>
2020-08-15 22:48:23 +1000

    Phase 1, integrated dialog pop-up on memberships page.

--- src/pages/Memberships.vue
+++ src/pages/Memberships.vue
@@ -12,16 +12,53 @@
         </div>
         <div class="col full-width" align="center">
         <q-btn
-            size="35px"
-            round
-            color="primary"
-            icon="add"
-            class="q-mt-md"
-            target="_blank"
-            @click="go('my-link')"
+          size="35px"
+          round
+          color="primary"
+          icon="add"
+          class="q-mt-md"
+          target="_blank"
+          @click="layout = true"
         />
         </div>
       </div>
+
+    <div class="q-pa-md q-gutter-sm">
+    <q-dialog v-model="layout">
+      <q-layout view="Lhh lpR fff" container class="bg-white">
+        <q-header class="bg-primary">
+          <q-toolbar>
+            <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+            <q-toolbar-title>Header</q-toolbar-title>
+            <q-btn flat @click="drawerR = !drawerR" round dense icon="menu" />
+            <q-btn flat v-close-popup round dense icon="close" />
+          </q-toolbar>
+        </q-header>
+
+        <q-footer class="bg-black text-white">
+          <q-toolbar inset>
+            <q-toolbar-title>Footer</q-toolbar-title>
+          </q-toolbar>
+        </q-footer>
+
+        <q-drawer bordered v-model="drawer" :width="200" :breakpoint="600" content-class="bg-grey-3 q-pa-sm">
+          <div v-for="n in 50" :key="n">Drawer {{ n }} / 50</div>
+        </q-drawer>
+
+        <q-drawer side="right" bordered v-model="drawerR" :width="200" :breakpoint="300" content-class="bg-grey-3 q-pa-sm">
+          <div v-for="n in 50" :key="n">Drawer {{ n }} / 50</div>
+        </q-drawer>
+
+        <q-page-container>
+          <q-page padding>
+            <p v-for="n in contentSize" :key="n">
+              {{ lorem }}
+            </p>
+          </q-page>
+        </q-page-container>
+      </q-layout>
+    </q-dialog>
+    </div>
   </q-page>
 </template>
 
@@ -44,9 +81,21 @@ export default {
   components: { Cards },
   data () {
     return {
-      cardsLinks: cardsData
+      cardsLinks: cardsData,
+      layout: false,
+      moreContent: true,
+      drawer: false,
+      drawerR: false,
+      lorem: 'hello, this is a test'
     }
   },
+
+  computed: {
+    contentSize () {
+      return this.moreContent ? 150 : 5
+    }
+  },
+
   methods: {
     go (href) {
       console.log(href)
