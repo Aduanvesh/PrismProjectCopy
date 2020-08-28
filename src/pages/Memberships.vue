2746fb9f59bef5f2d0ab0f77319e023bf4d34b28
polarbeared <edwardw1997@gmail.com>
2020-08-28 14:16:31 +1000

    Started linking memberships to database

--- src/pages/Memberships.vue
+++ src/pages/Memberships.vue
@@ -34,13 +34,6 @@
             <q-btn flat v-close-popup round dense icon="close" class="float-left" />
             </q-img>
         </q-header>
-
-        <q-footer class="bg-grey-3 text-black">
-          <q-toolbar>
-          <q-btn flat v-close-popup round icon="check_circle" class="float-right" />
-          </q-toolbar>
-        </q-footer>
-
         <q-drawer bordered v-model="drawer" :width="200" :breakpoint="600" content-class="bg-grey-3 q-pa-sm">
           <div v-for="n in 50" :key="n">Drawer {{ n }} / 50</div>
         </q-drawer>
@@ -48,49 +41,66 @@
         <q-drawer side="right" bordered v-model="drawerR" :width="200" :breakpoint="300" content-class="bg-grey-3 q-pa-sm">
           <div v-for="n in 50" :key="n">Drawer {{ n }} / 50</div>
         </q-drawer>
-
-        <q-page-container>
-          <!-- <q-page padding>
-            <p v-for="n in contentSize" :key="n">
-              {{ lorem }}
-            </p>
-          </q-page> -->
-        <q-page padding>
-          <q-field outlined label="Card Name" stack-label class="q-pa-sm">
-            <template v-slot:control>
-              <div class="self-center full-width no-outline" tabindex="0"> Name </div>
+          <q-page-container>
+          <q-page padding>
+          <q-form @submit= "createNewCard" class= "q-gutter-md">
+            <template>
+                  <q-input
+                    filled
+                    v-model="createNewCardForm.colour"
+                    class="my-input"
+                    label="Card Colour"
+                  >
+                    <template v-slot:append>
+                      <q-icon name="colorize" class="cursor-pointer">
+                        <q-popup-proxy transition-show="scale" transition-hide="scale">
+                          <q-color v-model="createNewCardForm.colour" />
+                        </q-popup-proxy>
+                      </q-icon>
+                    </template>
+                  </q-input>
             </template>
-          </q-field>
-          <q-field outlined label="Type" stack-label class='q-pl-sm q-pr-sm q-pb-sm'>
-            <template v-slot:control>
-              <div class="self-center full-width no-outline" tabindex="0"> Type </div>
-            </template>
-          </q-field>
-          <q-field outlined label="Price" stack-label class='q-pl-sm q-pr-sm q-pb-sm'>
-            <template v-slot:control>
-              <div class="self-center full-width no-outline" tabindex="0"> Price </div>
-            </template>
-          </q-field>
-          <q-field outlined label="Details" stack-label class='q-pl-sm q-pr-sm q-pb-sm'>
-            <template v-slot:control>
-              <div class="self-center full-width no-outline" tabindex="0"> Details </div>
-            </template>
-          </q-field>
-            <div align="absolute-bottom-right">
-              <q-toggle
-                v-model="first"
-                icon="alarm"
-                label="Send Alerts"
-              />
-              <q-toggle
-                v-model="second"
-                color="green"
-                icon="credit_card"
-                label="Card is Visible"
-              />
-            </div>
-        </q-page>
-        </q-page-container>
+              <q-field outlined label="Card Name" type="text" stack-label class="q-pa-sm" v-model="createNewCardForm.cardName">
+                <template v-slot:control>
+                  <div class="self-center full-width no-outline" tabindex="0"> Name </div>
+                </template>
+              </q-field>
+              <q-field outlined label="Type" stack-label class='q-pl-sm q-pr-sm q-pb-sm' v-model="createNewCardForm.cardType">
+                <template v-slot:control>
+                  <div class="self-center full-width no-outline" tabindex="0"> Type </div>
+                </template>
+              </q-field>
+              <q-field outlined label="Price" stack-label class='q-pl-sm q-pr-sm q-pb-sm' v-model="createNewCardForm.cardPrice">
+                <template v-slot:control>
+                  <div class="self-center full-width no-outline" tabindex="0"> Price </div>
+                </template>
+              </q-field>
+              <q-field outlined label="Details" stack-label class='q-pl-sm q-pr-sm q-pb-sm' v-model="createNewCardForm.cardDetails">
+                <template v-slot:control>
+                  <div class="self-center full-width no-outline" tabindex="0"> Details </div>
+                </template>
+              </q-field>
+                <div align="absolute-bottom-right">
+                  <q-toggle
+                    v-model="createNewCardForm.cardSendAlerts"
+                    icon="alarm"
+                    label="Send Alerts"
+                  />
+                  <q-toggle
+                    v-model="createNewCardForm.cardVisibility"
+                    color="green"
+                    icon="credit_card"
+                    label="Card is Visible"
+                  />
+                </div>
+            <q-footer class="bg-grey-3 text-black">
+              <q-toolbar>
+              <q-btn flat v-close-popup type="submit" round icon="check_circle" class="float-right" />
+              </q-toolbar>
+            </q-footer>
+          </q-form>
+          </q-page>
+          </q-page-container>
       </q-layout>
     </q-dialog>
     </div>
@@ -100,6 +110,7 @@
 <script>
 // Usually the import statement below is placed in a layouts vue, rather than a page.
 import Cards from 'components/Cards.vue'
+import { mapActions } from 'vuex'
 // import Modal from 'components/Modal.vue'
 const cardsData = [
   {
@@ -115,14 +126,22 @@ export default {
   components: { Cards },
   data () {
     return {
-      first: true,
-      second: true,
+      createNewCardForm: {
+        colour: '#ffffff',
+        cardName: '',
+        cardType: '',
+        cardPrice: '',
+        cardSendAlerts: true,
+        cardVisibility: true
+      },
       cardsLinks: cardsData,
       layout: false,
       moreContent: true,
       drawer: false,
       drawerR: false,
-      lorem: 'hello, this is a test'
+      lorem: 'hello, this is a test',
+      color: '#FF00FF',
+      secondColor: '#027be3'
     }
   },
 
@@ -133,9 +152,13 @@ export default {
   },
 
   methods: {
+    ...mapActions('store', ['createNewCard']),
     go (href) {
       console.log(href)
       alert('Hey')
+    },
+    createNewCard () {
+      this.createNewCard(this.createNewCardForm)
     }
   }
 }
