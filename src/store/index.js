3bbfdb61961213a7f833ca9640ef6321205a5c73
polarbeared <edwardw1997@gmail.com>
2020-10-22 22:17:24 +1000

    Something about routes.

--- src/store/index.js
+++ src/store/index.js
@@ -113,7 +113,10 @@ export default new Vuex.Store({
                       lastName: doc.data().last_name,
                       id: userID
                     })
-                  if (router.currentRoute.fullPath !== '/dashboard/user/'+userID && !router.currentRoute.fullPath.includes('profile') && !router.currentRoute.fullPath.includes('event')) {
+                  if (router.currentRoute.fullPath !== '/dashboard/user/'+userID 
+                  && !router.currentRoute.fullPath.includes('profile') 
+                  && !router.currentRoute.fullPath.includes('event')
+                  ) {
                     router.push('/dashboard/user/'+userID)
                   }
                 } else {
@@ -124,7 +127,7 @@ export default new Vuex.Store({
                     type: 'Club',
                     id: userID
                   })
-                  if (router.currentRoute.fullPath !== '/dashboard/club/'+userID && !router.currentRoute.fullPath.includes('profile') && !router.currentRoute.fullPath.includes('event')) {
+                  if (router.currentRoute.fullPath !== '/dashboard/club/'+userID && !router.currentRoute.fullPath.includes('profile') && !router.currentRoute.fullPath.includes('event') && !router.currentRoute.fullPath.includes('upload')) {
                     router.push('/dashboard/club/'+userID)
                   }
                 }
