88c940de595c34a319b54cf7b13d384ee04b0613
polarbeared <edwardw1997@gmail.com>
2020-10-23 14:17:49 +1000

    staging changes to components

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -16,13 +16,13 @@
 import Hero from "./components/Hero";
 import AboutUs from "./components/AboutUs";
 import Counts from "./components/Counts";
-import CustomControls from "./components/CustomControls";
-import Navigation from "./components/Navigation";
-import JavascriptComponents from "./components/JavascriptComponents";
-import Carousel from "./components/Carousel";
-import Icons from "./components/Icons";
-import Examples from "./components/Examples";
-import DownloadSection from "./components/DownloadSection";
+// import CustomControls from "./components/CustomControls";
+// import Navigation from "./components/Navigation";
+// import JavascriptComponents from "./components/JavascriptComponents";
+// import Carousel from "./components/Carousel";
+// import Icons from "./components/Icons";
+// import Examples from "./components/Examples";
+// import DownloadSection from "./components/DownloadSection";
 
 export default {
   name: "components",
@@ -30,13 +30,13 @@ export default {
     Hero,
     AboutUs,
     Counts,
-    CustomControls,
-    Navigation,
-    JavascriptComponents,
-    Carousel,
-    Icons,
-    Examples,
-    DownloadSection
+    // CustomControls,
+    // Navigation,
+    // JavascriptComponents,
+    // Carousel,
+    // Icons,
+    // Examples,
+    // DownloadSection
   }
 };
 </script>
